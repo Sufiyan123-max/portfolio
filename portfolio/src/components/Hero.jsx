caaa7b945c9c3598a5dcc31bdd1d3714970ed300
Sufiyan123-max <sifiyan100@gmail.com>
2025-02-19 10:33:14 +0530

    hii

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
               Contact With Me
             </button>
             <a
-              href="/resume.pdf" // Reference the resume in the public folder
+              href="/resume1.pdf" // Reference the resume in the public folder
               download // This triggers the download when clicked
               className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
               transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
